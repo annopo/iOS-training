5a793f9196030c52cd75947332dd30a25598fdeb
annopo <anna.ishizaki@hey.jp>
2021-05-27 16:17:28 +0900

    changed to display the weather using API

--- Yumemi-Weather/Yumemi-Weather/ViewController.swift
+++ Yumemi-Weather/Yumemi-Weather/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var reloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedReloadButton(_ sender: Any) {
+        let weather = YumemiWeather.fetchWeather()
+        
+        switch weather {
+        case "sunny":
+            weatherImageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.sunny()
+        case "cloudy":
+            weatherImageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.cloudy()
+        case "rainy":
+            weatherImageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.rainy()
+        default:
+            print("error")
+        }
+    }
 }
 
+extension UIColor{
+    static func sunny()->UIColor {
+        return UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1.0)
+    }
+
+    static func cloudy()->UIColor{
+        return UIColor.lightGray
+    }
+
+    static func rainy()->UIColor{
+        return UIColor(red: 51/255, green: 204/255, blue: 255/255, alpha: 1.0)
+    }
+}
