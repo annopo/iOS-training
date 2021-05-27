5bf80a0502969f922864866c7423942c2061f3aa
annopo <anna.ishizaki@hey.jp>
2021-05-27 14:14:15 +0900

    created extention of UIColor

--- Yumemi-Weather/Yumemi-Weather/ViewController.swift
+++ Yumemi-Weather/Yumemi-Weather/ViewController.swift
@@ -23,20 +23,31 @@ class ViewController: UIViewController {
         let weather = YumemiWeather.fetchWeather()
         
         switch weather {
-            case "sunny":
-                weatherImageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
-                weatherImageView.tintColor = UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1.0)
-
-            case "cloudy":
-                weatherImageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
-                weatherImageView.tintColor = .lightGray
-            case "rainy":
-                weatherImageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
-                weatherImageView.tintColor = UIColor(red: 51/255, green: 204/255, blue: 255/255, alpha: 1.0)
-
-            default:
-                print("error")
+        case "sunny":
+            weatherImageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.sunny()
+        case "cloudy":
+            weatherImageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.cloudy()
+        case "rainy":
+            weatherImageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = UIColor.rainy()
+        default:
+            print("error")
         }
     }
 }
 
+extension UIColor{
+    class func sunny()->UIColor {
+        return UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1.0)
+    }
+
+    class func cloudy()->UIColor{
+        return UIColor.lightGray
+    }
+
+    class func rainy()->UIColor{
+        return UIColor(red: 51/255, green: 204/255, blue: 255/255, alpha: 1.0)
+    }
+}
