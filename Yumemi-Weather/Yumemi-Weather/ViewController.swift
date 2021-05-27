c8e7e45ca4d472adc33a38b3014c60aafb4ba64d
annopo <anna.ishizaki@hey.jp>
2021-05-27 10:43:19 +0900

    added weather image

--- Yumemi-Weather/Yumemi-Weather/ViewController.swift
+++ Yumemi-Weather/Yumemi-Weather/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var reloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tappedReloadButton(_ sender: Any) {
+        let weather = YumemiWeather.fetchWeather()
+        
+        switch weather {
+            case "sunny":
+                weatherImageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
+                weatherImageView.tintColor = UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1.0)
 
+            case "cloudy":
+                weatherImageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
+                weatherImageView.tintColor = .lightGray
+            case "rainy":
+                weatherImageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
+                weatherImageView.tintColor = UIColor(red: 51/255, green: 204/255, blue: 255/255, alpha: 1.0)
+
+            default:
+                print("error")
+        }
+    }
 }
 
