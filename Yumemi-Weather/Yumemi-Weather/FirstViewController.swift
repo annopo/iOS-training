836f7c4f60df4e641563bbc0c4d4763b0dc8c604
annopo <anna.ishizaki@hey.jp>
2021-06-03 19:06:04 +0900

    added lifecycle of ViewController

--- Yumemi-Weather/Yumemi-Weather/FirstViewController.swift
+++ Yumemi-Weather/Yumemi-Weather/FirstViewController.swift
@@ -15,6 +15,10 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
--- Yumemi-Weather/Yumemi-Weather/ViewController.swift
+++ Yumemi-Weather/Yumemi-Weather/ViewController.swift
@@ -102,6 +102,16 @@ class ViewController: UIViewController {
             showAlert(title: "Error", message: "エラーが発生しました。")
         }
     }
+    
+    @IBAction func tappedCloseButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        presentingViewController?.beginAppearanceTransition(true, animated: animated)
+        presentingViewController?.endAppearanceTransition()
+    }
 }
 
 extension UIColor{
